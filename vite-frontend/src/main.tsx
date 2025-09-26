69b3d3002d373079805ee4440bb62225d5409beb
qaq <bqlpfy@gmail.com>
2025-09-26 20:04:07 +0800

    x修复反代

--- vite-frontend/src/main.tsx
+++ vite-frontend/src/main.tsx
@@ -1,16 +1,16 @@
 
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
 import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <HashRouter>
+  <BrowserRouter>
     <Provider>
       <App />
     </Provider>
-  </HashRouter>
+  </BrowserRouter>
 );
 
--- vite-frontend/vite.config.ts
+++ vite-frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
   ],
-  base: './',    
+  base: '/',    
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
